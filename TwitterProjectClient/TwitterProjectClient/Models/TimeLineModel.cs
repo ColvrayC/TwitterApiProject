abceb90a9108de6d6a4e2a9e71169292155f0c5f
Camille COLVRAY <camille.colvray@ynov.com>
2016-03-14 17:03:59 +0100

    timeline avec images

---
+++ TwitterProjectClient/TwitterProjectClient/Models/TimeLineModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterProjectClient.Models
+{
+    public class TimeLineModel
+    {
+        public string Author { get; set; }
+        public string HashTagAuthor { get; set; }
+        public string TimePosted { get; set; }
+    }
+}
